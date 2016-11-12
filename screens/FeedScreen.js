5fb08e8cdcac1d8d6c9b56305486f2e56e2dc5f7
meekay <mkolb@meekay.net>
2016-11-12 15:56:57 -0500

    Proof of concept for request interception.

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -23,9 +23,23 @@ export default class FeedScreen extends React.Component {
   render() {
     return (
       <WebView
-        source={require('./feed.html')}/>
+        source={require('./feed.html')}
+        onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest} />
     );
   }
+
+  onShouldStartLoadWithRequest = (event) => {
+    // Implement any custom loading logic here, don't forget to return!
+    let found = event.url.match(/pharos:\/\/video\/(\d+)/);
+
+    if (found) {
+      let videoId = found[1];
+      console.log("got pharos URL: " + videoId);
+      return false;
+    } else {
+      return true;
+    }
+  };
 
 }
 
